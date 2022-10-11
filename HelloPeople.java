0f692ac2ac42cf0df4233a00fc627299357fc5ef
fahriandormd <115510434+fahriandormd@users.noreply.github.com>
2022-10-11 12:41:35 +0700

    HelloPeople.java

---
+++ HelloPeople.java
@@ -0,0 +1,9 @@
+public class HelloPeople {
+    /**
+    * Program pertama saya Sopwatun Anisa
+    */
+   public static void main(String[] args) {
+      //Menampilkan kata "Welcome to java programming" dilayar
+       System.out.println("Welcome to java programming!");
+   }
+}
