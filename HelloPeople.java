5b150eee9d9253eacd24df68682f779e586cf25a
fahriandormd <115510434+fahriandormd@users.noreply.github.com>
2022-10-11 12:42:17 +0700

    Menambahkan nama

--- HelloPeople.java
+++ HelloPeople.java
@@ -3,7 +3,7 @@ public class HelloPeople {
     * Program pertama saya Sopwatun Anisa
     */
    public static void main(String[] args) {
-      //Menampilkan kata "Welcome to java programming" dilayar
-       System.out.println("Welcome to java programming!");
+      //Menampilkan kata "Welcome to java programming Sopwatun" dilayar
+       System.out.println("Welcome to java programming Sopwatun!");
    }
 }
